858cb8e03d80ca8742127a6f80a36e3354a904a4
Santiago Fernández <lostwalle268@hotmail.com>
2023-10-08 23:13:57 -0500

    Reloj e interfaz

---
+++ GAME/Assets/ControladorJuego.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ControladorJuego : MonoBehaviour
+{
+
+[SerializeField] private float tiempoMax;
+[SerializeField] private Slider slider;
+private float TiempoActual;
+private bool TiempoActivado = false;
+
+private void Start(){
+   ActivarTemp();
+}
+private void Update(){
+    if (TiempoActivado){
+        CambiarContador();
+    }
+}
+private void CambiarContador(){
+   TiempoActual -= Time.deltaTime;
+    if(TiempoActual >= 0){
+        slider.value= TiempoActual;
+    }
+    if (TiempoActual <=0){
+        Debug.Log("Derrota");
+        CambiarTemp(false);
+    }
+}
+private void CambiarTemp(bool estado){
+    TiempoActivado = estado;
+}
+public void ActivarTemp(){
+    TiempoActual = tiempoMax;
+    slider.maxValue=tiempoMax;
+    CambiarTemp(true);
+}
+public void DesactivarTemp(){
+    CambiarTemp(false);
+}
+
+}
--- GAME/Assets/Scripts/Movimiento.cs
+++ GAME/Assets/Scripts/Movimiento.cs
@@ -8,7 +8,7 @@ public float runspeed=2;
 Rigidbody2D rb2D;
     void Start()
     {
-        rb2D= GetComponent<Rigidbody2D>();
+        rb2D=GetComponent<Rigidbody2D>();
     }
     void FixedUpdate()
     {
@@ -19,16 +19,6 @@ Rigidbody2D rb2D;
             rb2D.velocity= new Vector2(-runspeed,rb2D.velocity.y);
         }else{
         rb2D.velocity= new Vector2(0,rb2D.velocity.y);
-        }
-
-
-        if(Input.GetKey("w")||Input.GetKey("up")){
-            rb2D.velocity= new Vector2(rb2D.velocity.x,runspeed);
-        }
-        else if(Input.GetKey("s")||Input.GetKey("down")){
-            rb2D.velocity= new Vector2(rb2D.velocity.x,-runspeed);
-        }else{
-        rb2D.velocity= new Vector2(rb2D.velocity.x,0);
         }
         
     } 
