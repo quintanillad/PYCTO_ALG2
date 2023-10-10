b4afea31d0c355556e664e5e2ef4e27171b59bb9
Santiago Fernández <lostwalle268@hotmail.com>
2023-10-09 21:58:17 -0500

    Vidas y boceto de asteroides

---
+++ GAME/Assets/GameManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+     public static GameManager Instance { get; private set; }
+    
+	public HUD hud;
+
+    public int PuntosTotales {get; private set;}
+
+	private int vidas = 5;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.Log("Cuidado! Mas de un GameManager en escena.");
+        }
+    }
+	public void PerderVida() {
+		vidas -= 1;
+		hud.DesactivarVida(vidas);
+        if(vidas == 0)
+		{
+			// Reiniciamos el nivel.
+			SceneManager.LoadScene(0);
+		}
+	}
+
+	public bool RecuperarVida() {
+		if (vidas == 5)
+		{
+			return false;
+		}
+
+		hud.ActivarVida(vidas);
+		vidas += 1;
+		return true;
+	}
+   
+   }
---
+++ GAME/Assets/HUD.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUD : MonoBehaviour
+{
+	public GameObject[] vidas;
+	public void DesactivarVida(int indice) {
+		vidas[indice].SetActive(false);
+	}
+
+	public void ActivarVida(int indice) {
+		vidas[indice].SetActive(true);
+	}
+}
